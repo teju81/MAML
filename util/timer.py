dc67deafffa7a207e30f6de42d6bb5f47cb5abf0
teju81 <teju81@gmail.com>
2019-03-13 06:53:34 +0530

    Create timer.py

---
+++ util/timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer(object):
+    def __init__(self, message):
+        self.message = message
+
+    def __enter__(self):
+        self.time_start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        new_time = time.time() - self.time_start
+        fname, lineno, method, _ = tb.extract_stack()[-2]  # Get caller
+        _, fname = os.path.split(fname)
+        id_str = '%s:%s' % (fname, method)
+        print('TIMER:'+color_string('%s: %s (Elapsed: %fs)' % (id_str, self.message, new_time), color='gray'))
