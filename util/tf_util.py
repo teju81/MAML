891fcf462333c5daff22bb59e531228f766d45e1
teju81 <teju81@gmail.com>
2019-03-13 07:04:19 +0530

    Create tf_util.py

---
+++ util/tf_util.py
@@ -0,0 +1,44 @@
+## Network helpers
+def safe_get(name, *args, **kwargs):
+    try:
+        return tf.get_variable(name, *args, **kwargs)
+    except ValueError:
+        tf.get_variable_scope().reuse_variables()
+        return tf.get_variable(name, *args, **kwargs)
+
+def conv_block(inp, cweight, bweight, is_training, reuse, scope, dropout_rate = 0.05, activation=tf.nn.relu, max_pool_pad='VALID', residual=False):
+    """ Perform, conv, batch norm, nonlinearity, and max pool """
+    stride, no_stride = [1,2,2,1], [1,1,1,1]
+
+    if FLAGS.max_pool:
+        conv_output = tf.nn.conv2d(inp, cweight, no_stride, 'SAME') + bweight
+    else:
+        conv_output = tf.nn.conv2d(inp, cweight, stride, 'SAME') + bweight
+    normed = normalize(conv_output, activation, reuse, scope)
+    
+    if FLAGS.reg == 'dropout':
+      if is_training:
+        normed = tf.nn.dropout(normed, rate=dropout_rate)
+      else:
+        normed = tf.add(normed, 0)
+    
+    if FLAGS.max_pool:
+        normed = tf.nn.max_pool(normed, stride, stride, max_pool_pad)
+    return normed
+
+def normalize(inp, activation, reuse, scope):
+    if FLAGS.norm == 'batch_norm':
+        return tf_layers.batch_norm(inp, activation_fn=activation, reuse=reuse, scope=scope)
+    elif FLAGS.norm == 'layer_norm':
+        return tf_layers.layer_norm(inp, activation_fn=activation, reuse=reuse, scope=scope)
+    elif FLAGS.norm == 'None':
+        if activation is not None:
+            return activation(inp)
+        else:
+            return inp
+
+## Loss functions
+def mse(pred, label):
+    pred = tf.reshape(pred, [-1])
+    label = tf.reshape(label, [-1])
+    return tf.reduce_mean(tf.square(pred-label))
